60013eb2feba18bc64ce64e3b7c91b5660bffb90
Kaihsin-Lin-Designer <kaihsin.lin.id@gmail.com>
2024-11-30 12:19:59 +0800

    adding 2 custom features

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # develop page
 
 This is a single page showing the current site/page is underdevelop.  
-You can add your **email** in function ***switchDevEmail()*** from ***main.js***.
+- You can add your **email** in function ***switchDevEmail()*** from ***main.js***.  
+- You can add your **site name** in function ***switchSiteName()*** from ***main.js***.  
+- You can add your **image** in function ***switchPicture()*** from ***main.js***.  
--- main.js
+++ main.js
@@ -5,6 +5,37 @@ const switchDevEmail = (email) => {
     }
     const domElement = document.querySelector("#dev-email");
     domElement.innerText = email;
+    return;
+}
+
+const switchSiteName = (siteNameEn, siteNameZh) => {
+    const enEls = document.querySelectorAll(".site-name-en");
+    const zhEls = document.querySelectorAll(".site-name-zh");
+    if(!siteNameEn || !siteNameZh) {
+        enEls.forEach((enEl) => {
+            enEl.innerText = "The Site";
+        })
+        zhEls.forEach((zhEl) => {
+            zhEl.innerText = "網站";
+        })
+        return;
+    }
+    enEls.forEach((enEl) => {
+        enEl.innerText = siteNameEn;
+    })
+    zhEls.forEach((zhEl) => {
+        zhEl.innerText = siteNameZh;
+    })
+    return;
+}
+
+const switchPicture = (imgSrc, imgAlt) => {
+    const imgEl = document.querySelector("#img");
+    imgEl.setAttribute("src", imgSrc);
+    imgEl.setAttribute("alt", imgAlt);
+    return;
 }
 
 switchDevEmail("kaihsin.lin.id@gmail.com");
+switchSiteName("NCKU Badminton Official Website", "成大羽球隊官網");
+switchPicture("/picture/ncku-badminton-logo-1.svg", "NCKU Badminton Logo")
